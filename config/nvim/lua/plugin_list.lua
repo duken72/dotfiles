09d50ec66467c1a7840b5b27c85ec013f6e87dd9
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-04-21 12:12:47 +0200

    Clean-up configuration for nvim-autopairs

--- config/nvim/lua/plugin_list.lua
+++ config/nvim/lua/plugin_list.lua
@@ -1,5 +1,4 @@
 -- Here is where you install your plugins.
---  You can configure plugins using the `config` key--[[ . --[[ ]] ]]
 require('lazy').setup({
   -- Nerd fonts
   "nvim-tree/nvim-web-devicons",
@@ -71,20 +70,8 @@ require('lazy').setup({
         end,
       },
 
-      { -- autopairing of (){}[] etc
-        "windwp/nvim-autopairs",
-        opts = {
-          fast_wrap = {},
-          disable_filetype = { "TelescopePrompt", "vim" },
-        },
-        config = function(_, opts)
-          require("nvim-autopairs").setup(opts)
-
-          -- setup cmp for autopairs
-          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end,
-      },
+      -- autopairing of (){}[] etc
+      "windwp/nvim-autopairs",
 
       { -- cmp sources plugins
         "saadparwaiz1/cmp_luasnip",
--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -2,7 +2,7 @@
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
-luasnip.config.setup {}
+luasnip.config.setup({})
 
 cmp.setup {
   snippet = {
@@ -48,12 +48,14 @@ local npairs = require("nvim-autopairs")
 local Rule = require('nvim-autopairs.rule')
 
 npairs.setup({
-    check_ts = true,
-    ts_config = {
-        lua = {'string'},-- it will not add a pair on that treesitter node
-        javascript = {'template_string'},
-        java = false,-- don't check treesitter on java
-    }
+  fast_wrap = {},
+  disable_filetype = { "TelescopePrompt", "vim" },
+  check_ts = true,
+  ts_config = {
+    lua = {'string'},-- it will not add a pair on that treesitter node
+    javascript = {'template_string'},
+    java = false,-- don't check treesitter on java
+  },
 })
 
 local ts_conds = require('nvim-autopairs.ts-conds')
@@ -67,7 +69,4 @@ npairs.add_rules({
 })
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
