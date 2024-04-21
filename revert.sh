286dd6a4bc7de5f94d95954c5643d7ae90f7d9c1
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-04-21 16:45:49 +0200

    Format all BASH scripts

--- revert.sh
+++ revert.sh
@@ -5,11 +5,11 @@
 
 DATE="$1"
 echo "Revert pacman updates on $DATE"
-grep -a $DATE /var/log/pacman.log | grep upgraded > /tmp/upd.txt
-gawk '{gsub(/\(/,""); gsub(/\)/,""); print $4"-"$5}' /tmp/upd.txt > /tmp/pkgs.txt
+grep -a $DATE /var/log/pacman.log | grep upgraded >/tmp/upd.txt
+gawk '{gsub(/\(/,""); gsub(/\)/,""); print $4"-"$5}' /tmp/upd.txt >/tmp/pkgs.txt
 
 cd /var/cache/pacman/pkg/
 for pkg in $(cat /tmp/pkgs.txt); do
-    # sudo pacman --noconfirm -U "$i"*.zst;
-    sudo pacman -U "$pkg"*.zst;
+	# sudo pacman --noconfirm -U "$i"*.zst;
+	sudo pacman -U "$pkg"*.zst
 done
