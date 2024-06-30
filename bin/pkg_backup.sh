d5b0d33dc4f5fe6faaf99b9cb678f9cc019466bd
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-06-30 12:05:12 +0200

    Refactor scripts, update packages backup info

--- bin/pkg_backup.sh
+++ bin/pkg_backup.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
-
 PKG_DIR=/home/duken72/.dotfiles/pkg
-# This command will be run with super user privilege
-# In that case, $USER will be root
-# PKG_DIR=/home/$USER/dotfiles/pkg
 
-# Clear content
+# Clear existing content in certain files (for later concatenation)
 >${PKG_DIR}/pkg_pacman.txt
+>${PKG_DIR}/pkg_pacman.new
 >${PKG_DIR}/pkg_group.txt
 
-# Collect packages in package groups
+# Collect packages in following package groups
 PKG_GROUPS=("base-devel" "xfce4" "xfce4-goodies" "xorg")
 for PKG_GROUP in "${PKG_GROUPS[@]}"; do
-    pacman -Sg $PKG_GROUP | sd -s "${PKG_GROUP} " "" >>${PKG_DIR}/pkg_group.txt
+	pacman -Sg $PKG_GROUP | sd -s "${PKG_GROUP} " "" >>${PKG_DIR}/pkg_group.txt
 done
 
 # Backup packages
-pacman -Qq | grep texlive- >${PKG_DIR}/pkg_latex.txt
-pacman -Qetq | grep python- >${PKG_DIR}/pkg_pacman_python.txt
-pacman -Qem | grep -v texlive- >${PKG_DIR}/pkg_aura.txt
-pacman -Qten | grep -v texlive- | grep -v python- >>${PKG_DIR}/pkg_pacman.txt
+pacman -Qq | grep texlive >${PKG_DIR}/pkg_latex.txt
+pacman -Qeq | grep python- >${PKG_DIR}/pkg_pacman_python.txt
+pacman -Qeqm | grep -v debug >${PKG_DIR}/pkg_aura.txt
+pacman -Qem | grep -v debug >${PKG_DIR}/pkg_aura.new
+pacman -Qtnq | grep -v python- | grep -v texlive >>${PKG_DIR}/pkg_pacman.txt
+pacman -Qtn | grep -v python- | grep -v texlive >>${PKG_DIR}/pkg_pacman.new
 # Filter packages that are installed via pkg groups
 while read pkg; do
-    sed -i "/$pkg/d" ${PKG_DIR}/pkg_pacman.txt
+	sed -i "/$pkg/d" ${PKG_DIR}/pkg_pacman.txt
+	sed -i "/$pkg/d" ${PKG_DIR}/pkg_pacman.new
 done <${PKG_DIR}/pkg_group.txt
-
-# Reinstall when needed
-# check pkg/pacman_install.sh
-# xargs sudo apt -y install < packages.txt
-
-# To specify package version, if they exist in /var/cache/pacman/pkg
-# pacman -S --needed - < pkg_pacman.txt
