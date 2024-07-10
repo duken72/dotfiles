039795e2d080e9295ac4eda8c142fa46ddd89344
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-07-10 21:04:57 +0200

    Update script to check which packages were updated

--- pkg/delta.sh
+++ pkg/delta.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 clear
-delta pkg_aura.old pkg_aura.new
-delta pkg_pacman.old pkg_pacman.new
+if [ -f pkg_aura.old ] && [ -f pkg_aura.new ]; then
+	delta pkg_aura.old pkg_aura.new
+fi
+if [ -f pkg_pacman.old ] && [ -f pkg_pacman.new ]; then
+	delta pkg_pacman.old pkg_pacman.new
+fi
