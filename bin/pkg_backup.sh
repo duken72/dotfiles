ecf806788b535b086d08bbd0e44891f37fd77c69
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-07-20 15:43:32 +0200

    Back up

--- bin/pkg_backup.sh
+++ bin/pkg_backup.sh
@@ -4,13 +4,13 @@ PKG_DIR=/home/duken72/.dotfiles/pkg
 # Clear existing content in certain files (for later concatenation)
 >${PKG_DIR}/pkg_pacman.txt
 >${PKG_DIR}/pkg_pacman.new
->${PKG_DIR}/pkg_group.txt
+# >${PKG_DIR}/pkg_group.txt
 
 # Collect packages in following package groups
-PKG_GROUPS=("base-devel" "xfce4" "xfce4-goodies" "xorg")
-for PKG_GROUP in "${PKG_GROUPS[@]}"; do
-	pacman -Sg $PKG_GROUP | sd -s "${PKG_GROUP} " "" >>${PKG_DIR}/pkg_group.txt
-done
+# PKG_GROUPS=("base-devel" "xfce4" "xfce4-goodies" "xorg")
+# for PKG_GROUP in "${PKG_GROUPS[@]}"; do
+# 	pacman -Sg $PKG_GROUP | sd -s "${PKG_GROUP} " "" >>${PKG_DIR}/pkg_group.txt
+# done
 
 # Backup packages
 pacman -Qq | grep texlive >${PKG_DIR}/pkg_latex.txt
