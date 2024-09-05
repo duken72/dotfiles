8d922c916598eafcf133c4ca8928979cbf3a9610
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-09-05 22:08:12 +0200

    Back up

--- bin/ytmp3.sh
+++ bin/ytmp3.sh
@@ -3,7 +3,7 @@
 echo "3 args: links, start time point, end time point"
 # Download the video from YouTube
 yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 $1 -o temp.mp3 &&
-    # trim and transform it to MP3
-    ffmpeg -ss $2 -to $3 -i temp.mp3 -y -vn -c copy file.mp3 &&
-    # Clean up
-    rm temp.* && clear && echo "Download mp3 completed."
+	# trim and transform it to MP3
+	ffmpeg -ss $2 -to $3 -i temp.mp3 -y -vn -c copy "$4.mp3" &&
+	# Clean up
+	rm temp.* && clear && echo "Downloaded: $4.mp3"
