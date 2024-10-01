eb5a7663b25422888a1ffe0948aca766afccae04
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-10-01 22:16:23 +0200

    Update NetworkManager config

--- config/NetworkManager/run.sh
+++ config/NetworkManager/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ABSOLUTE_PARENT_PATH=$(realpath $(dirname $BASH_SOURCE))
-sudo ln -svf $ABSOLUTE_PARENT_PATH/*.conf -t /etc/NetworkManager/conf.d
+sudo ln -svf $ABSOLUTE_PARENT_PATH/conf.d -t /etc/NetworkManager
 sudo ln -svf $ABSOLUTE_PARENT_PATH/svg/nm*.svg /usr/share/icons/hicolor/scalable/apps
 sudo ln -svf $ABSOLUTE_PARENT_PATH/png/nm*.png -t /usr/share/icons/hicolor/22x22/apps
