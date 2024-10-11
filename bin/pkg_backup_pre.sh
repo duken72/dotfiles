c3847037c48a34dbddd82e75738f8d541c999a7d
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-10-11 19:14:55 +0200

    Back up

--- bin/pkg_backup_pre.sh
+++ bin/pkg_backup_pre.sh
@@ -2,12 +2,12 @@
 PKG_DIR=/home/duken72/.dotfiles/pkg
 
 # Clear existing content in certain files (for later concatenation)
->${PKG_DIR}/pkg_pacman.old
+>${PKG_DIR}/.pkg_pacman.old
 
 # Backup packages
-pacman -Qem | grep -v debug >${PKG_DIR}/pkg_aura.old
-pacman -Qtn | grep -v python- | grep -v texlive >>${PKG_DIR}/pkg_pacman.old
+pacman -Qem | grep -v debug >${PKG_DIR}/.pkg_aura.old
+pacman -Qtn | grep -v python- | grep -v texlive >>${PKG_DIR}/.pkg_pacman.old
 # Filter packages that are installed via pkg groups
 while read pkg; do
-	sed -i "/$pkg/d" ${PKG_DIR}/pkg_pacman.old
+	sed -i "/$pkg/d" ${PKG_DIR}/.pkg_pacman.old
 done <${PKG_DIR}/pkg_group.txt
