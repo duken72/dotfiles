f855f4d0da8c87233ecc67d55828f1642d7d0122
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-11-21 13:09:41 +0100

    Update script to install packages in Ubuntu

--- pkg/Ubuntu/apt_install.sh
+++ pkg/Ubuntu/apt_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # xargs sudo apt -y install < packages.txt
+sudo apt update
 sudo apt install -y vim git tree tmux fd-find zsh \
 	neofetch htop ranger terminator ncdu \
 	slack curl
@@ -9,7 +10,7 @@ sudo apt install -y vim git tree tmux fd-find zsh \
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
+sudo apt install -y brave-browser
 
 # Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -20,6 +21,7 @@ rm -rf ~/.fzf
 # Install JetBrains Mono Nerd Font
 cd ~/Downloads
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+mkdir -p JetBrainsMonoNLNerdFont
 tar xvf JetBrainsMono.tar.xz -C ~/Downloads/JetBrainsMonoNLNerdFont
 sudo mv ~/Downloads/JetBrainsMonoNLNerdFont /usr/share/fonts
 
@@ -43,3 +45,4 @@ chmod u+x nvim.appimage
 # Exposing nvim globally.
 sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+sudo apt install -y npm lua5.3
