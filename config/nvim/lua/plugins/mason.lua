58b8ef3dcad5a444d6ab699e02cc3d1df7424f93
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2025-06-02 18:13:14 +0200

    Back up

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -110,15 +110,15 @@ mason_lspconfig.setup({
 	ensure_installed = vim.tbl_keys(servers),
 })
 
-mason_lspconfig.setup_handlers({
-	function(server_name)
-		require("lspconfig")[server_name].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = servers[server_name],
-		})
-	end,
-})
+-- mason_lspconfig.setup_handlers({
+-- 	function(server_name)
+-- 		require("lspconfig")[server_name].setup({
+-- 			capabilities = capabilities,
+-- 			on_attach = on_attach,
+-- 			settings = servers[server_name],
+-- 		})
+-- 	end,
+-- })
 
 require("mason-tool-installer").setup({
 	ensure_installed = {
