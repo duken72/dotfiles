02676552e7c0ae50c7ea92bb2df457e579271c30
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2025-10-16 11:11:24 +0200

    Update script to revert pacman package updates

--- bin/revert-updates.sh
+++ bin/revert-updates.sh
@@ -3,6 +3,7 @@
 # This script reverts all pacman updates on specified date (and possibly time)
 # if you still keep the packages cache in /var/cache/pacman/pkg
 
+# Date should be in this format: yyyy-mm-dd
 DATE="$1"
 echo "Revert pacman updates on $DATE"
 grep -a $DATE /var/log/pacman.log | grep upgraded >/tmp/upd.txt
@@ -10,6 +11,5 @@ gawk '{gsub(/\(/,""); gsub(/\)/,""); print $4"-"$5}' /tmp/upd.txt >/tmp/pkgs.txt
 
 cd /var/cache/pacman/pkg/
 for pkg in $(cat /tmp/pkgs.txt); do
-	# sudo pacman --noconfirm -U "$i"*.zst;
-	sudo pacman -U "$pkg"*.zst
+	sudo pacman --needed --noconfirm -U "$pkg"*.zst
 done
