5cf92c3382996322c2bfcc3b0198882ee17eb2d6
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2025-11-18 11:08:53 +0100

    Update script install nvim config: install suitable clipboard tool depending on window manager

--- config/nvim/run.sh
+++ config/nvim/run.sh
@@ -3,5 +3,10 @@
 # Questionable: vale shfmt glow
 sudo pacman -S --needed --noconfirm neovim npm luarocks \
     tree-sitter-bash tree-sitter-cli tree-sitter-python shfmt
+if [ -n "$WAYLAND_DISPLAY" ] || [ -S /run/user/$(id -u)/wayland-0 ]; then
+    sudo pacman -S --needed wl-clipboard
+else
+    sudo pacman -S --needed xclip
+fi
 ABSOLUTE_PARENT_PATH=$(realpath $(dirname $BASH_SOURCE))
 ln -svf $ABSOLUTE_PARENT_PATH -t ~/.config
