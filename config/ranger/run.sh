0f751ec3ebcbf5f3d3a81c9cafacb8d6ef7c2ab6
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2025-11-22 10:27:33 +0100

    Minor refactor

--- config/ranger/run.sh
+++ config/ranger/run.sh
@@ -3,7 +3,13 @@
 sudo pacman -Sy --needed --noconfirm ranger docx2txt ueberzugpp \
     ffmpegthumbnailer ffmpeg atool python-pipx
 ABSOLUTE_PARENT_PATH=$(realpath $(dirname $BASH_SOURCE))
+rm -rf ~/.config/ranger
 ln -svf $ABSOLUTE_PARENT_PATH -t ~/.config
 
 # Install nbread to preview python notebook with ranger
-pipx install git+https://github.com/tnwei/nbread
+pipx list | grep nbread
+if [ $? -eq 0 ]; then
+    echo "Already install nbread."
+else
+    pipx install git+https://github.com/tnwei/nbread
+fi
